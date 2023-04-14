ee56855603b50863467a85abe90e0f554178f4df
Solrac_B <carlitosaburgosj1609@gmail.com>
2023-04-14 08:05:44 -0500

    update

--- reto1.php
+++ reto1.php
@@ -7,7 +7,7 @@ for ($i = 1; $i <= 10; $i++) {
     echo"<tr><td><font color='red'>.$i.</font></td><tr>";
     
 }
-for ($i = 1; $i <= 10; $i++) {
+for ($i = 2; $i <= 10; $i=2) {
     echo"<tr><td><font color='blue'>.$i.</font></td><tr> ";
 }
 
